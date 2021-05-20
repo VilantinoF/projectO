6be3dbaf6df2e7ebc37e413eea7b429796b43d4f
vilantinof <vilantino11@gmail.com>
2021-05-21 01:32:44 +0700

    menambah session untuk 900.php

--- 900.php
+++ 900.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+
+if (!isset($_SESSION["choosen"])) {
+    header("Location: kepegawaian.php");
+    exit;
+}
 
 if (isset($_POST["next"])) {
     $value = $_POST["pegawai"];
