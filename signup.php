4efba76215e1c2ac7bfb3dbdc7e3d3621b0d82db
vilantinof <vilantino11@gmail.com>
2021-05-26 22:11:36 +0700

    membuat tampilan halaman signup

---
+++ signup.php
@@ -0,0 +1,42 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <form action="" method="POST">
+            <div class="form-box">
+                <div class="form-input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="form-input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <button class="button" type="submit" name="signup">Sign Up!</button>
+            </div>
+        </form>
+
+    </div>
+
+
+</body>
+
+</html>
